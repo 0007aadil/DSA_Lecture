5b61e32b1745cea537510a4f91142bea67976551
0007aadil <aadilahsan007@gmail.com>
2025-01-30 02:47:20 +0530

    30 jan 2025

---
+++ Question_1.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Question_1 {
+
+    public static boolean monotonic(ArrayList<Integer> list) {
+        boolean increasing = true, decreasing = true;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (list.get(i) > list.get(i + 1)) {
+                increasing = false;
+            }
+            if (list.get(i) < list.get(i + 1)) {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println("List: " + list);
+        System.out.println("Is monotonic? " + monotonic(list));
+    }
+}
---
+++ Question_2.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Question_2 {
+
+    public static ArrayList<Integer> lonelyNum(ArrayList<Integer> list) {
+        if (list.size() == 1) {
+            return new ArrayList<>(list); // If there's only one number, return it
+        }
+
+        Collections.sort(list); // Sort the list first
+
+        ArrayList<Integer> newList = new ArrayList<>();
+
+        // Check first element
+        if (list.get(0) + 1 < list.get(1)) {
+            newList.add(list.get(0));
+        }
+
+        // Check middle elements
+        for (int i = 1; i < list.size() - 1; i++) {
+            if (list.get(i - 1) + 1 < list.get(i) && list.get(i) + 1 < list.get(i + 1)) {
+                newList.add(list.get(i));
+            }
+        }
+
+        // Check last element
+        if (list.get(list.size() - 2) + 1 < list.get(list.size() - 1)) {
+            newList.add(list.get(list.size() - 1));
+        }
+
+        return newList;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(6);
+        list.add(5);
+        list.add(8);
+
+        System.out.println("Input List: " + list);
+        System.out.println("Lonely Numbers: " + lonelyNum(list));
+    }
+}
