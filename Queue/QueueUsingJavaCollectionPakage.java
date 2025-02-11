5d5a4d41edd05b91744a3fcd2796828bd1f72440
0007aadil <aadilahsan007@gmail.com>
2025-02-11 21:46:56 +0530

    11/02/2025

---
+++ Queue/QueueUsingJavaCollectionPakage.java
@@ -0,0 +1,28 @@
+package Queue;
+
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueUsingJavaCollectionPakage {
+    
+    public static void main(String[] args) {
+        // Queue<Integer> q = new LinkedList<>() { 
+        Queue<Integer> q = new ArrayDeque<>(); //ArrayDequeue
+            
+        
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        System.out.println(q.remove());
+        q.add(4);
+        System.out.println(q.remove());
+        q.add(5);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
