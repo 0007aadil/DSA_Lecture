cc5b806b06ff6e145e909f50a80622b39181560f
0007aadil <aadilahsan007@gmail.com>
2025-02-18 22:44:55 +0530

    18 feb 2025

---
+++ BinaryTree/InOrder.java
@@ -0,0 +1,61 @@
+package BinaryTree;
+
+public class InOrder {
+    
+
+    static class Node {
+    
+        int data ;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+
+    static class BTree {
+
+        static int idx = -1;
+    
+        public static Node buildTree(int nodes[]){
+
+            idx++;
+            if(nodes[idx] == -1){
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+
+        }
+
+        public static void InOdr(Node root){
+            if (root == null) {
+                return;
+            }
+            InOdr(root.left);
+            System.out.print(root.data+ " ");
+            InOdr(root.right);
+            
+        }
+    }
+    
+
+
+    public static void main(String[] args) {
+        int nodes[] = { 1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+
+        BTree tree = new BTree();
+        Node root = tree.buildTree(nodes);
+        
+
+        tree.InOdr(root);
+
+    }
+}
