ceee6b9e1ef5a26d362ef602ccc1bbaf4966e0c1
0007aadil <aadilahsan007@gmail.com>
2025-03-02 02:16:29 +0530

    3 march 2025

---
+++ Hashing/syntax.java
@@ -0,0 +1,23 @@
+package Hashing;
+
+import java.util.HashMap;
+
+public class syntax {
+    
+
+
+
+
+    public static void main(String[] args) {
+        //create 
+        HashMap<String,Integer> hm = new HashMap<>();
+
+        //Insert
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("US", 50);
+        hm.put("SriLanka", 112);
+
+        System.out.println(hm);
+    }
+}
