63843eb06ecf04fc7ff805cf75886dd4cea181e9
0007aadil <aadilahsan007@gmail.com>
2025-03-02 02:29:20 +0530

    02 march 2025

--- Hashing/syntax.java
+++ Hashing/syntax.java
@@ -17,12 +17,29 @@ public class syntax {
         hm.put("China", 150);
         hm.put("US", 50);
         hm.put("SriLanka", 112);
-
-
+       
+        // Get 
         int Population = hm.get("India");
-        
 
-        System.out.println( Population);
+        //ConstainsKey
+        System.out.println(hm.containsKey("India"));     // true 
+        System.out.println( hm.containsKey("Japan")); // False
+
+        //Remove 
+        System.out.println(hm.remove("China"));
+        System.out.println(hm.remove("Hola"));
+        System.out.println(hm);
+
+        //Size 
+
+        System.out.println(hm.size());
+
+        //Is Empty 
+        System.out.println(hm.isEmpty());
+
+        //clear
+        hm.clear();
+        System.out.println(hm.isEmpty());
 
 
     }
