b78c95f0cfc028a525a42bea8dac995d02406453
0007aadil <aadilahsan007@gmail.com>
2025-03-02 15:47:43 +0530

    03 march 2025

---
+++ Hashing/TreeMapImplementation.java
@@ -0,0 +1,21 @@
+package Hashing;
+
+import java.util.TreeMap;
+
+public class TreeMapImplementation {
+
+    public static void main(String[] args) {
+
+        TreeMap<String,Integer> tm = new TreeMap<>();
+        tm.put("Inida", 100);
+        tm.put("japan", 211);
+        tm.put("China", 99);
+        tm.put("sriLanka", 87);
+
+        System.out.println(tm);
+        
+    }
+    
+
+    
+}
