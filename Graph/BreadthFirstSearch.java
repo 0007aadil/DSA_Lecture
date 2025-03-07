6d722c8220ef6c2cb3db676465d5167cea14f064
0007aadil <aadilahsan007@gmail.com>
2025-03-07 23:25:23 +0530

    07 march 2025

---
+++ Graph/BreadthFirstSearch.java
@@ -0,0 +1,19 @@
+package Graph;
+
+public class BreadthFirstSearch {
+    static class  Edge {
+    
+        int src;
+        int dest;
+        int wt;
+
+        public Edge(int s, int d , int w){
+            this.dest=d;
+            this.src=s;
+            this.wt =w;
+        }
+    } 
+
+
+    
+}
