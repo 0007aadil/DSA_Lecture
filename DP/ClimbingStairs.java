75b3235c9c6f75a03c9a9e544af974f0069483b8
0007aadil <aadilahsan007@gmail.com>
2025-03-15 03:02:52 +0530

    15 MARch 2025

--- DP/ClimbingStairs.java
+++ DP/ClimbingStairs.java
@@ -16,29 +16,48 @@ public class ClimbingStairs {
     // }
 
     // Memoization  --> O(n)
-     public static int countWays(int n , int ways[]){
-        if (n==0) {
-            return 1;
-        }
-        if (n<0) {
-            return 0;
-        }
+    //  public static int countWays(int n , int ways[]){
+    //     if (n==0) {
+    //         return 1;
+    //     }
+    //     if (n<0) {
+    //         return 0;
+    //     }
 
-        if (ways[n] != -1) { // aleady counted 
-            return ways[n];
+    //     if (ways[n] != -1) { // aleady counted 
+    //         return ways[n];
+            
+    //     }
+    //     ways[n] = countWays(n-1, ways) + countWays(n-2, ways);
+    //    return ways[n];
+    // }
+
+
+    //Tabulation ---> O(n)
+
+    public static int countWaysTab(int n){
+        int dp[] = new int[n+1]; // 0 to n
+        dp[0] = 1;
+
+        for(int i =1 ; i <= n; i++){
+            if(i == 1){
+                dp[i] = dp[i-1];
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
             
         }
-        ways[n] = countWays(n-1, ways) + countWays(n-2, ways);
-       return ways[n];
+        return dp[n];
+
     }
 
     
 
     public static void main(String[] args) {
-        int n= 500; // n=3 -> & n =4 -> 5 => 8
+        int n= 5; // n=3 -> & n =4 -> 5 => 8
         int ways[] = new int[n+1]; // 0, 0, 0, 0, 0
         Arrays.fill(ways, -1); // this gives values to arr ---> -1,  -1, -1, -1, -1
-        System.out.println(countWays(n , ways));
+        System.out.println(countWaysTab(n));
 
     }
 }
