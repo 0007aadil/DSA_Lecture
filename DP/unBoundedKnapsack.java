089fd07d82ff52c8dd0ea31c00d4cfc09e105dbe
0007aadil <aadilahsan007@gmail.com>
2025-03-18 22:06:44 +0530

    18 MARch

--- DP/unBoundedKnapsack.java
+++ DP/unBoundedKnapsack.java
@@ -15,7 +15,7 @@ public class unBoundedKnapsack {
         for(int i=1; i<n+1; i++){
             for(int j=1; j<W+1; j++){
                 if (wt[i-1] <= j) { //valid
-                    dp[i][j] = Math.max(val[i-1] + dp[i][j-wt[i-1]], dp[i-1][j]);
+                    dp[i][j] = Math.max(val[i-1] + dp[i   ][j-wt[i-1]], dp[i-1][j]);
                     
                 }else { // invalid
                     dp[i][j] = dp[i-1][j];
