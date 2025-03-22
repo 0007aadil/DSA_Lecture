38d2b6c889146e1a132fabe397adf280523f93c7
0007aadil <aadilahsan007@gmail.com>
2025-03-23 00:34:48 +0530

    23 March 2025

---
+++ DP/catalanNumberTabulation.java
@@ -0,0 +1,27 @@
+package DP;
+
+public class catalanNumberTabulation {
+
+    public static int catalanTab(int n ){ //0(n * n)
+
+        int dp[] = new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+
+        for(int i=2 ; i<=n ; i++){ //Ci
+            for(int j =0 ; j<i; j++){
+                dp[i] += dp[j] * dp[i-j-1];
+
+            }
+
+        }
+        return dp[n];
+    }
+    
+
+    public static void main(String[] args) {
+        int n=4;
+
+        System.out.println(catalanTab(n));
+    }
+}
