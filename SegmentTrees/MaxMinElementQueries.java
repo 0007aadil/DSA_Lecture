0b0a49c99f393bf1cff509837f67f6e99bb693a6
0007aadil <aadilahsan007@gmail.com>
2025-03-24 23:01:17 +0530

    24 March

--- SegmentTrees/MaxMinElementQueries.java
+++ SegmentTrees/MaxMinElementQueries.java
@@ -22,6 +22,27 @@ public class MaxMinElementQueries {
         tree[i] = Math.max(tree[2*i+1], tree[2*i+2]);
     }
 
+    public static int getmax(int arr[] , int qi , int qj){
+        int n = arr.length;
+        return getMaxUtil(0, 0, n-1, qi, qj);
+    }
+
+    public static int getMaxUtil(int i , int si , int sj ,int qi , int qj){
+
+        if (si > qj || sj < qi) { // no-overlap
+            return Integer.MIN_VALUE;
+        } else if (si >= qi || sj <= qj) {// complete overlap
+            return tree[i];
+        }else { // partial overlap
+
+            int mid = (si + sj)/2;
+            int leftAns = getMaxUtil(2*i+1, si, mid, qi, qj);
+            int rightAns = getMaxUtil(2*i+2, mid+1, sj, qi, qj);
+            return Math.max(leftAns, rightAns);
+
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {6,8,-1,2,17,1,3,2,4};
         int n = arr.length;
@@ -29,8 +50,12 @@ public class MaxMinElementQueries {
 
         buildTree(0, 0, n-1, arr);
 
-        for(int i=0; i<tree.length ;  i++){
-            System.out.print(tree[i] + " ");
-        }
+        // for(int i=0; i<tree.length ;  i++){
+        //     System.out.print(tree[i] + " ");
+        // }
+
+        int max = getmax(arr, 2, 5);
+        System.out.println(max);
+
     }
 }
